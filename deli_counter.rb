62a6c2b7e326cca6d559d7b30775b3e32d996487
Chris Beyer <beyercorpuz@gmail.com>
2020-02-01 21:42:48 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(deli_line)
      counter = 0
      phrase = "The line is currently: "
       while counter < deli_line.length
-      phrase += "#{counter +1}. #{deli_line[0]}"
+      phrase += "#{counter +1}. #{deli_line[0]} "
       counter += 1
       end
     puts phrase
