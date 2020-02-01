077b5f723c3fb66cadf1205888b572c60424f2d9
Chris Beyer <beyercorpuz@gmail.com>
2020-02-01 21:38:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,33 @@
 # Write your code here.
-def line(place)
-if  place >= 1 
- puts  "Your place in line is #{place}"
- if place < 1 
-  puts " The line is currently empty"
-end
+def line(deli_line)
+  if deli_line == []
+     puts "The line is currently empty."
+   else  
+     counter = 0
+     phrase = "The line is currently: "
+      while counter < deli_line.length
+      phrase += "#{counter+1}. #{deli_line[0]}"
+      counter += 1
+      end
+    puts phrase
+    end
 end
 
-end
   def take_a_number(deli,name)
     puts "The name of the person is #{name}"
   end
-    puts "Their place in line is #{place}"
-  
+    
     def now_servicing(name)
       puts " The next person in line is #{name}"
-    end
       name.shift
-      if #{name} < 1 
-      puts " There is nobody waiting to be served!"
-  end
+    end
+      
+     # if #{name} < 1 
+   #   puts " There is nobody waiting to be served!"
+ #
+ 
   
-I#
+
+
+  
+
