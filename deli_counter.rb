657aed0418024bbcef70a805f42ace71c23a4fce
Chris Beyer <beyercorpuz@gmail.com>
2020-02-01 14:46:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 # Write your code here.
 def line(place)
-If  place >= 1 
+if  place >= 1 
  puts  "Your place in line is #{place}"
 end
-If place < 1 
+if place < 1 
   puts " The line is currently empty"
   def take_a_number(deli,name)
     puts "The name of the person is #{name}"
@@ -13,6 +13,6 @@ If place < 1
     def now_servicing(name)
       puts " The next person in line is #{name}"
       name.shift
-      If #{name} < 1 puts " There is nobody waiting to be served!"
+      if #{name} < 1 puts " There is nobody waiting to be served!"
     end
   
